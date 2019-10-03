c355bf82cb32792648ab350b6038bf5951ea8193
Yanfei <ren105@purdue.edu>
2019-10-03 00:30:46 -0700

    Create multi_power_method.jl
    101.602080 seconds (624.97 k allocations: 499.235 MiB, 0.13% gc time)

---
+++ power_method/multi_power_method.jl
@@ -0,0 +1,64 @@
+using StaticArrays
+using LinearAlgebra
+using SparseArrays
+using LinearAlgebra
+using StaticArrays
+using DataStructures
+using MatrixNetworks
+
+function pagerank_power_multi!(x::Vector{T}, y::Vector{T},
+    P, alpha, v, tol,
+    maxiter::Int) where T
+    ialpha = 1.0/(1.0-alpha)
+    xinit = x
+    fill!(x, zero(T))
+    _applyv!(x,v,0.0,ones(T)) # iteration number 0
+    for iter=1:maxiter
+        mul!(y,P,x,1.0,0.0)
+        gamma = 1.0-alpha*sum(y)
+
+        delta = zero(T)
+        _applyv!(y,v,alpha,gamma)
+        @simd for i=1:length(x)
+            @inbounds delta += abs.(y[i] - x[i])
+        end
+        x,y = y,x # swap
+        if all(delta*ialpha .< tol)
+            break
+        end
+    end
+    if !(x === xinit) # x is not xinit, so we need to copy it over
+        xinit[:] .= x
+    end
+    xinit # always return xinit
+end
+
+function _applyv!(x::Vector, v::SArray, alpha::T, gamma) where T
+    @simd for i in 1:length(x)
+        @inbounds x[i] *= alpha
+    end
+
+    for i in 1:length(v)
+      ev = SVector{length(v),T}((i == j ? 1 : 0 for j=1:length(v))...)
+      x[v[i]] += gamma[i]*ev
+    end
+end
+
+multi_pagerank(P, alpha, v, tol) = pagerank_power_multi!(
+    Vector{SVector{length(v),Float64}}(undef, size(P,1)),
+    Vector{SVector{length(v),Float64}}(undef, size(P,1)),
+    P, alpha, v, tol, ceil(Int, log(tol)/log(alpha)))
+
+multi_pagerank(P, alpha, v) = multi_pagerank(P,alpha,v,(1.0-alpha)/size(P,1))
+
+function load_data()
+    A = MatrixNetworks.readSMAT("/p/mnt/data/graph-db/snap/soc-LiveJournal1-scc.smat")
+    AI,AJ,AV = findnz(A)
+    d = vec(sum(A;dims=2))
+    n = size(A,1)
+    Pt = sparse(AI,AJ,AV./d[AI],n,n) # form the row-stochastic version of P
+    return A, Pt
+end
+A, Pt = load_data()
+
+@time y = multi_pagerank(Pt', 0.85, SVector((1 : 8)...))
