3f8de93ceb7363d5c7f650e331486014d4d50668
Yanfei <ren105@purdue.edu>
2019-10-03 00:33:40 -0700

    Create simple_power_method.jl
    Converged on iteration 78
     39.798718 seconds (183.10 k allocations: 67.803 MiB, 0.12% gc time)

---
+++ power_method/simple_power_method.jl
@@ -0,0 +1,56 @@
+using SparseArrays
+using LinearAlgebra
+using StaticArrays
+using DataStructures
+using MatrixNetworks
+import KahanSummation
+
+function load_data()
+    A = MatrixNetworks.readSMAT("/p/mnt/data/graph-db/snap/soc-LiveJournal1-scc.smat")
+    AI,AJ,AV = findnz(A)
+    d = vec(sum(A;dims=2))
+    n = size(A,1)
+    Pt = sparse(AI,AJ,AV./d[AI],n,n) # form the row-stochastic version of P
+    return A, Pt
+end
+
+function pagerank_power!(x::Vector{T}, y::Vector{T},
+    P, alpha::T, v, tol::T,
+    maxiter::Int) where T
+    ialpha = 1.0/(1.0-alpha)
+    xinit = x
+    fill!(x, 0.0)
+    _applyv!(x,v,0.0,1.0) # iteration number 0
+    for iter=1:maxiter
+        mul!(y,P,x)
+        gamma = 1.0-alpha*KahanSummation.sum_kbn(y)
+        delta = 0.0
+        _applyv!(y,v,alpha,gamma)
+        @simd for i=1:length(x)
+            @inbounds delta += abs(y[i] - x[i]) # TODO implement Kahan summation
+        end
+        x,y = y,x # swap
+        if delta*ialpha < tol
+                    println("Converged on iteration $iter")
+            break
+        end
+    end
+    if !(x === xinit) # x is not xinit, so we need to copy it over
+        xinit[:] .= x
+    end
+    xinit # always return xinit
+end
+
+function _applyv!(x::Vector{T}, v::Integer,
+                 alpha::T, gamma::T) where T
+    @simd for i in 1:length(x)
+        @inbounds x[i] *= alpha
+    end
+    x[v] += gamma
+end
+simple_pagerank(P, alpha, v::Int, tol) = pagerank_power!(
+    Vector{Float64}(undef, size(P,1)), Vector{Float64}(undef, size(P,1)),
+    P, alpha, v, tol, ceil(Int, log(tol)/log(alpha)))
+simple_pagerank(P, alpha, v::Int) = simple_pagerank(P,alpha,v,(1.0-alpha)/size(P,1))
+A, Pt = load_data()
+@time x = simple_pagerank(Pt', 0.85, 1)
