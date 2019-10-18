cef399c093b89862e743a907766eddefd63352e1
David Gleich <dgleich@purdue.edu>
2019-10-18 09:58:48 -0400

    Benchmark updates

--- benchmark.jl
+++ benchmark.jl
@@ -28,6 +28,7 @@ function warmup_methods()
     A = load_matrix_network("airports")
     fill!(A.nzval, 1.0)
     A,Pt = ManyPagerank.normalize_data(A)
+    At = copy(A')
     pr = seeded_pagerank(A, 0.85, 1, 1/1e6)
 
     println("Run simple_pagerank for power_method")
@@ -52,13 +53,15 @@ function warmup_methods()
     y = ManyPagerank.GaussSeidelMultiPR(Pt, 0.85, SVector((1 : 8)...))
     #x = ManyPagerank.multi_pagerank(Pt', 0.85, SVector((1 : 8)...))
 
+    println("Run fast_gauss")
+    ManyPagerank.multi_gauss_seidel_from_zero(A, 0.85, SVector((1 : 8)...))
+
     println("Run simple_push_method")
-    x = ManyPagerank.simple_push_method(A, 0.85, 1)
+    x = ManyPagerank.simple_push_method(At, 0.85, 1)
 
     println("Run multi_push_method")
-    x = ManyPagerank.multi_push_method(A, 0.85, SVector((1 : 8)...))
+    x = ManyPagerank.multi_push_method(At, 0.85, SVector((1 : 8)...))
 
-    At = copy(A')
 
     println("Run cyclic_push_method")
     x = ManyPagerank.cyclic_push_method(At, 0.85, 1)
@@ -66,48 +69,60 @@ function warmup_methods()
     println("Run cyclic_multi_push_method")
     x = ManyPagerank.cyclic_multi_push_method(At, 0.85, SVector((1 : 8)...))
 
+
+
+
 end
 
 function run_methods(graphfile::AbstractString)
 
     A,Pt = ManyPagerank.load_data(graphfile)
+    At = copy(A')
     d = vec(sum(A,dims=2))
     id = 1.0 ./d
 
+    display(size(A))
+
+    benchmarks = Dict{String,Any}()
+
+
+
     println("Benchmarking simple_pagerank for power_method")
-    b1 = @benchmark ManyPagerank.simple_pagerank($Pt',0.85,1)
+    benchmarks["power_simple"] = @benchmark ManyPagerank.simple_pagerank($Pt',0.85,1)
 
     println("Benchmarking fast_pagerank_power for power_method")
-    b2 = @benchmark ManyPagerank.fast_pagerank_power($A, 0.85, 1)
+    benchmarks["power_fast"] = @benchmark ManyPagerank.fast_pagerank_power($A, 0.85, 1)
 
     println("Benchmarking multi_pagerank for power_method")
-    b3 = @benchmark ManyPagerank.multi_pagerank($Pt', 0.85, SVector((1 : 8)...))
+    benchmarks["power_multi"] = @benchmark ManyPagerank.multi_pagerank($Pt', 0.85, SVector((1 : 8)...))
 
     println("Benchmarking FastGaussSeidel")
-    b4 = @benchmark ManyPagerank.FastGaussSeidel($A, $id, $d, 0.85, 1)
+    benchmarks["gs_fast"] = @benchmark ManyPagerank.FastGaussSeidel($A, $id, $d, 0.85, 1)
 
     println("Benchmarking FastGaussSeidelFromZero")
-    b5 = @benchmark ManyPagerank.FastGaussSeidelFromZero($A, $id, $d, 0.85, 1)
+    benchmarks["gs_fast_zero"] = @benchmark ManyPagerank.FastGaussSeidelFromZero($A, $id, $d, 0.85, 1)
 
     println("Benchmarking GaussSeidelMultiPR")
-    b6 = @benchmark ManyPagerank.GaussSeidelMultiPR($Pt, 0.85, SVector((1 : 8)...))
+    benchmarks["gs_multi"] = @benchmark ManyPagerank.GaussSeidelMultiPR($Pt, 0.85, SVector((1 : 8)...))
+
+    println("Benchmarking fast_gauss")
+    benchmarks["gs_multi_zero"] = @benchmark ManyPagerank.multi_gauss_seidel_from_zero($A, 0.85, SVector((1 : 8)...))
+
 
     println("Benchmarking simple_push_method")
-    b7 = @benchmark ManyPagerank.simple_push_method($A, 0.85, 1)
+    benchmarks["push_simple"] = @benchmark ManyPagerank.simple_push_method($At, 0.85, 1)
 
     println("Benchmarking multi_push_method")
-    b8 = @benchmark ManyPagerank.multi_push_method($A, 0.85, SVector((1 : 8)...))
-
-    At = copy(A')
+    benchmarks["push_multi"] =  @benchmark ManyPagerank.multi_push_method($At, 0.85, SVector((1 : 8)...))
 
     println("Benchmarking cyclic_push_method")
-    b9 = @benchmark ManyPagerank.cyclic_push_method($At, 0.85, 1)
+    benchmarks["push_cyclic"] = @benchmark ManyPagerank.cyclic_push_method($At, 0.85, 1)
 
     println("Benchmarking cyclic_multi_push_method")
-    b10 = @benchmark ManyPagerank.cyclic_multi_push_method($At, 0.85, SVector((1 : 8)...))
+    benchmarks["push_cyclic_multi"] = @benchmark ManyPagerank.cyclic_multi_push_method($At, 0.85, SVector((1 : 8)...))
 
     # Todo: format the benchmarking output info
-    return b1, b2, b3, b4, b5, b6, b7, b8, b9, b10
+    return benchmarks
 
 end
 
@@ -122,7 +137,8 @@ function main()
     @time warmup_methods()
 
     println("Running methods on $(parsed_args["graphfile"]) ")
-    run_methods(parsed_args["graphfile"])
+    display(run_methods(parsed_args["graphfile"]))
+    println("")
 
 
 end
---
+++ benchmark_dgleich_linux.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+JULIA=~/bin/julia-1.3.0-rc4/bin/julia bash runbenchmark.sh $@
