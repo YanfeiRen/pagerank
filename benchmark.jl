0a5ca9fc22c19ac3dabe26eb4bf220c0073509c6
YanfeiRen <ren105@purdue.edu>
2019-10-17 23:29:23 -0700

    Add benchmarkings

--- benchmark.jl
+++ benchmark.jl
@@ -23,19 +23,49 @@ end
 
 using MatrixNetworks
 using StaticArrays
+using BenchmarkTools
 function warmup_methods()
     A = load_matrix_network("airports")
     fill!(A.nzval, 1.0)
     A,Pt = ManyPagerank.normalize_data(A)
-    pr = seeded_pagerank(A,0.85,1,1/1e6)
-    x = ManyPagerank.simple_pagerank(Pt',0.85,1)
+    pr = seeded_pagerank(A, 0.85, 1, 1/1e6)
+
+    println("Run simple_pagerank for power_method")
+    x = ManyPagerank.simple_pagerank(Pt', 0.85, 1)
+
+    println("Run fast_pagerank_power for power_method")
+    x = ManyPagerank.fast_pagerank_power(A, 0.85, 1)
+
+    println("Run multi_pagerank for power_method")
     x = ManyPagerank.multi_pagerank(Pt', 0.85, SVector((1 : 8)...))
 
     d = vec(sum(A,dims=2))
     id = 1.0 ./d
+
+    println("Run FastGaussSeidel")
     x = ManyPagerank.FastGaussSeidel(A, id, d, 0.85, 1)
+
+    println("Run FastGaussSeidelFromZero")
+    x = ManyPagerank.FastGaussSeidelFromZero(A, id, d, 0.85, 1)
+
+    println("Run GaussSeidelMultiPR")
     y = ManyPagerank.GaussSeidelMultiPR(Pt, 0.85, SVector((1 : 8)...))
     #x = ManyPagerank.multi_pagerank(Pt', 0.85, SVector((1 : 8)...))
+
+    println("Run simple_push_method")
+    x = ManyPagerank.simple_push_method(A, 0.85, 1)
+
+    println("Run multi_push_method")
+    x = ManyPagerank.multi_push_method(A, 0.85, SVector((1 : 8)...))
+
+    At = copy(A')
+
+    println("Run cyclic_push_method")
+    x = ManyPagerank.cyclic_push_method(At, 0.85, 1)
+
+    println("Run cyclic_multi_push_method")
+    x = ManyPagerank.cyclic_multi_push_method(At, 0.85, SVector((1 : 8)...))
+
 end
 
 function run_methods(graphfile::AbstractString)
@@ -44,13 +74,41 @@ function run_methods(graphfile::AbstractString)
     d = vec(sum(A,dims=2))
     id = 1.0 ./d
 
-    @time x = ManyPagerank.simple_pagerank(Pt',0.85,1)
-    @time x = ManyPagerank.multi_pagerank(Pt', 0.85, SVector((1 : 8)...))
+    println("Benchmarking simple_pagerank for power_method")
+    b1 = @benchmark ManyPagerank.simple_pagerank($Pt',0.85,1)
 
-    d = vec(sum(A,dims=2))
-    id = 1.0 ./d
-    @time x = ManyPagerank.FastGaussSeidel(A, id, d, 0.85, 1)
-    @time y = ManyPagerank.GaussSeidelMultiPR(Pt, 0.85, SVector((1 : 8)...))
+    println("Benchmarking fast_pagerank_power for power_method")
+    b2 = @benchmark ManyPagerank.fast_pagerank_power($A, 0.85, 1)
+
+    println("Benchmarking multi_pagerank for power_method")
+    b3 = @benchmark ManyPagerank.multi_pagerank($Pt', 0.85, SVector((1 : 8)...))
+
+    println("Benchmarking FastGaussSeidel")
+    b4 = @benchmark ManyPagerank.FastGaussSeidel($A, $id, $d, 0.85, 1)
+
+    println("Benchmarking FastGaussSeidelFromZero")
+    b5 = @benchmark ManyPagerank.FastGaussSeidelFromZero($A, $id, $d, 0.85, 1)
+
+    println("Benchmarking GaussSeidelMultiPR")
+    b6 = @benchmark ManyPagerank.GaussSeidelMultiPR($Pt, 0.85, SVector((1 : 8)...))
+
+    println("Benchmarking simple_push_method")
+    b7 = @benchmark ManyPagerank.simple_push_method($A, 0.85, 1)
+
+    println("Benchmarking multi_push_method")
+    b8 = @benchmark ManyPagerank.multi_push_method($A, 0.85, SVector((1 : 8)...))
+
+    At = copy(A')
+
+    println("Benchmarking cyclic_push_method")
+    b9 = @benchmark ManyPagerank.cyclic_push_method($At, 0.85, 1)
+
+    println("Benchmarking cyclic_multi_push_method")
+    b10 = @benchmark ManyPagerank.cyclic_multi_push_method($At, 0.85, SVector((1 : 8)...))
+
+    # Todo: format the benchmarking output info
+    return b1, b2, b3, b4, b5, b6, b7, b8, b9, b10
+
 end
 
 function main()
