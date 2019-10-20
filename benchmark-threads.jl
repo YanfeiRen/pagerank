a466f75e5cc2937de6319e1f1266a3a8377cd398
David Gleich <dgleich@purdue.edu>
2019-10-20 15:40:23 -0400

    run all methods for threads

--- benchmark-threads.jl
+++ benchmark-threads.jl
@@ -10,6 +10,9 @@ function parse_commandline()
             help = "the total time in minutes"
             arg_type = Float64
             default = 14.4
+        "--nowarmup"
+            help = "an option without argument, i.e. a flag"
+            action = :store_false
         "graphfile"
             help = "a positional argument"
             required = true
@@ -28,9 +31,49 @@ function threaded_benchmark(graphfile::AbstractString, maxtime::Float64=14.4*60)
     vex = SVector((1:8)...)
     alpha = 0.85
 
+    println("Benchmarking simple_pagerank for power_method")
     benchmarks["power_simple"] = benchmark_method_single(maxtime, setup_call_simple_power, gdata, alpha, 1)
+    println(benchmarks["power_simple"])
 
+    println("Benchmarking fast_pagerank_power for power_method")
+    benchmarks["power_fast"] = benchmark_method_single(maxtime, setup_call_fast_power, gdata, alpha, 1)
+    println(benchmarks["power_fast"])
+
+    println("Benchmarking multi_pagerank for power_method")
     benchmarks["power_multi"] = benchmark_method_multi(maxtime, setup_call_simple_power_multi, gdata, alpha, vex)
+    println(benchmarks["power_multi"])
+
+    println("Benchmarking FastGaussSeidel")
+    benchmarks["gs_fast"] = benchmark_method_single(maxtime, setup_call_gs_fast, gdata, alpha, 1)
+    println(benchmarks["gs_fast"])
+
+    println("Benchmarking FastGaussSeidelFromZero")
+    benchmarks["gs_fast_zero"] = benchmark_method_single(maxtime, setup_call_gs_from_zero, gdata, alpha, 1)
+    println(benchmarks["gs_fast_zero"])
+
+    println("Benchmarking GaussSeidelMultiPR")
+    benchmarks["gs_multi"] = benchmark_method_multi(maxtime, setup_call_gs_multi, gdata, alpha, vex)
+    println(benchmarks["gs_multi"])
+
+    println("Benchmarking gs_multi_zero")
+    benchmarks["gs_multi_zero"] = benchmark_method_multi(maxtime, setup_call_gs_multi_from_zero, gdata, alpha, vex)
+    println(benchmarks["gs_multi_zero"])
+
+    println("Benchmarking simple_push_method")
+    benchmarks["push_simple"] = benchmark_method_single(maxtime, setup_call_simple_push_method, gdata, alpha, 1)
+    println(benchmarks["push_simple"])
+
+    println("Benchmarking multi_push_method")
+    benchmarks["push_multi"] = benchmark_method_multi(maxtime, setup_call_multi_push_method, gdata, alpha, vex)
+    println(benchmarks["push_multi"])
+
+    println("Benchmarking cyclic_push_method")
+    benchmarks["push_cyclic"] = benchmark_method_single(maxtime, setup_call_cyclic_push_method, gdata, alpha, 1)
+    println(benchmarks["push_cyclic"])
+
+    println("Benchmarking cyclic_multi_push_method")
+    benchmarks["push_cyclic_multi"] = benchmark_method_multi(maxtime, setup_call_cyclic_multi_push_method, gdata, alpha, vex)
+    println(benchmarks["push_cyclic_multi"])
 
     return benchmarks
 end
@@ -43,8 +86,10 @@ function main()
         println("  $arg  =>  $val")
     end
 
-    #println("Warming up methods ... ")
-    #@time warmup_methods()
+    if parsed_args["warmup"]
+        println("Warming up methods ... ")
+        @time warmup_methods()
+    end
 
     println("Running methods on $(parsed_args["graphfile"]) ")
     display(threaded_benchmark(parsed_args["graphfile"], 60*parsed_args["maxtime"]))
